a22e69347a0074f9f1ea37b70f1ae4ab7383f3de
usernotfree <lanzallen28@gmail.com>
2025-06-07 18:44:30 +0800

    Update stack_queue.h

--- stack_queue.h
+++ stack_queue.h
@@ -1,73 +1,83 @@
-// stack_queue.h
-#ifndef STACK_QUEUE_H
-#define STACK_QUEUE_H
-#include <string>
+// HEADER FILE - stack_queue.h
 
-const int MAX_SIZE = 5;
+#ifndef STACK_QUEUE_H // if STACK_QUEUE_H is not defined,
+#define STACK_QUEUE_H // define STACK_QUEUE_H to prevent multiple inclusions
+#include <string> // string header file
+using namespace std; // to not use std manually
+
+const int MAX_SIZE = 5; // define a constant for the maximum size of the static stack and queue (5 was set to be the maximum)
 
 // Static Stack
 class StaticStack {
-private:
-    std::string stack[MAX_SIZE];
-    int top;
-public:
-    StaticStack();
-    bool isFull();
-    bool isEmpty();
-    void push(const std::string& item);
-    void pop();
-    void display();
+    
+private: // static stack members that is/are protected
+    string stack[MAX_SIZE]; // an array that will store stack elements
+    int top;  // index of the top element inside the stack
+    
+public: // static stack operations
+    StaticStack(); // static stack constructor (initializes the stack)
+    bool isFull(); // checks if the static stack is full
+    bool isEmpty(); // checks if the static stack is empty
+    void push(const string& item); // push function
+    void pop(); // pop function
+    void display(); // display the elements of the static stack
 };
 
 // Dynamic Stack
-struct StackNode {
-    std::string data;
-    StackNode* next;
+struct StackNode { // structure named StackNode
+    string data; // data that is held by the node
+    StackNode* next; // a pointer to the next node in the stack
 };
 
-class DynamicStack {
-private:
-    StackNode* top;
+class DynamicStack { // class named DynamicStack
+
+private: // dynamic stack data members that is/are protected
+    StackNode* top; // a pointer to the top node inside the stack
+    
 public:
-    DynamicStack();
-    ~DynamicStack();
-    void push(const std::string& item);
-    void pop();
-    void display();
-    bool isEmpty();
+    DynamicStack(); // dynamic stack constructor (initializes the stack)
+    ~DynamicStack(); // dynamic stack destructor (cleans/frees dynamically allocated memory)
+    void push(const string& item); // push function
+    void pop(); // pop function
+    void display(); // display the elements of the dynamic stack
+    bool isEmpty(); // checks if the dynamic stack is empty
 };
 
 // Static Queue
 class StaticQueue {
-private:
-    std::string queue[MAX_SIZE];
-    int front, rear;
-public:
-    StaticQueue();
-    bool isFull();
-    bool isEmpty();
-    void enqueue(const std::string& caller);
-    void dequeue();
-    void display();
+    
+private: // static queue data members that is/are protected
+    string queue[MAX_SIZE]; // an array that will store the elements queued
+    int front, rear; // front and rear of the queue
+    
+public: // static queue operations
+    StaticQueue(); // static queue constructor (initializes the stack)
+    bool isFull(); // checks if the static stack is full
+    bool isEmpty(); // checks if the static stack is empty
+    void enqueue(const string& caller); // enqueue function 
+    void dequeue(); // dequeue function
+    void display(); // display the elements of the static queue
 };
 
 // Dynamic Queue
-struct QueueNode {
-    std::string caller;
-    QueueNode* next;
+struct QueueNode { // structure named QueueNode
+    string caller; // the data that is held by the node
+    QueueNode* next;  // a pointer to the next node in the queue
 };
 
 class DynamicQueue {
 private:
-    QueueNode* front;
-    QueueNode* rear;
+    QueueNode* front; // a pointer to the front of the queue
+    QueueNode* rear; // a pointer to the rear/end of the queue
+    
+// operations   
 public:
-    DynamicQueue();
-    ~DynamicQueue();
-    void enqueue(const std::string& caller);
-    void dequeue();
-    void display();
-    bool isEmpty();
+    DynamicQueue(); // dynamic queue constructor (initializes the stack)
+    ~DynamicQueue(); // dynamic queue destructor (cleans/frees dynamically allocated memory)
+    void enqueue(const string& caller); // adds an item to the rear of the queue
+    void dequeue(); // removes the data inputted at the front of the queue
+    void display(); // display the elements of the dynamic queue
+    bool isEmpty(); // checks if the dynamic queue is empty
 };
 
-#endif
+#endif  //  closes the #ifndef (for prevention of multiple inclusions)
